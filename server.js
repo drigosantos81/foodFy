5c27ded7dac2f834899c65f96245f7627d2da698
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-19 09:30:10 -0300

    Alteração no projeto para os padrões Backend.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+server.use(express.static('img'));
+
+server.set("view engine", "html");
+
+nunjucks.configure("pages", {
+    express: server
+});
+
+server.get("/", function(req, res) {
+    return res.render("index");
+})
+
+server.get("/sobre", function(req, res) {
+    return res.render("sobre");
+})
+
+server.listen(5000, function() {
+    console.log("Server Activeted");
+});
