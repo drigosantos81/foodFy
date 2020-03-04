e92f104e3ab2f4fd6c8972367435506917c29e2e
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-03 21:19:42 -0300

    Modal com problema no retorno do resultado. object HTMLCollection.

--- public/scripts.js
+++ public/scripts.js
@@ -1,54 +1,21 @@
 const modal = document.querySelector('.modal-menu');
 const grids = document.querySelectorAll('.grid-prato');
 const modalOn = document.querySelector('.modal-on');
-var recipeName;  
 
 for (let grid of grids) {
     grid.addEventListener("click", function() {
         const imageId = grid.getAttribute("id");
+        const titleId = grid.getElementsByClassName("author");
         modal.classList.add("active");
-        modal.querySelector("iframe").src=`/${imageId}.png`; 
-        setTextsToModalQuerys(imageId);
-        modal.querySelectorAll('p')[1].textContent = recipeName;
-    })
-}
+        modal.querySelector("iframe").src=`/${imageId}.png`;
+        // if (imageId == ) {
+            modal.querySelector("p").textContent = `por ${titleId}`;
+        // }        
+  });
+};
 
 document.querySelector('.fechar-modal').addEventListener("click", function(event) {
     event.preventDefault();
     modal.classList.remove("active");
     modal.querySelector("iframe").src="";
-})
-
-function setTextToModalQuery(grid){ // hardcode
-    switch (grid) {
-      case 'burger':
-        recipeName = "Triplo bacon burger"
-        autor = "Jorge Relato"
-        break;
-      case 'pizza':
-        recipeName = "Pizza 4 estações"
-        autor = "Fabiana Melo"
-      break;
-      case 'espaguete':
-        recipeName = "Espaguete ao molho"
-        autor = "Júlia Kinoto"
-      break;
-      case 'lasanha':
-        recipeName = "Lasanha mac n' cheese"
-        autor = "Juliano Vieira"
-      break;
-      case 'doce':
-        recipeName = "Docinhos pão-do-céu"
-        autor = "Ricardo Golvea"
-      break;
-      case 'asinha':
-        recipeName = "Asinhas de frango ao barbecue"
-        autor = "Vania Steroski"
-      break;
-    
-      default:
-  
-    break;
-    }
-    return 0;
-}
+});
