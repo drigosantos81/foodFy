e037ec8d1173553f14e21e6941e5d64e879414e6
Rodrigo Emanuel <drigosantos@gmail.com>
2020-08-13 12:12:22 -0300

    Alterada pasta de salvamento de imagens.

--- src/app/middlewares/multer.js
+++ src/app/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './img');
+        cb(null, './img/imagesUploaded');
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now().toString()}-${file.originalname}`);
