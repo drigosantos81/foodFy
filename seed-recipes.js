a7108f8f2bb847fa3b3b9d93b7b07307f42c44f0
Rodrigo Emanuel <drigosantos@gmail.com>
2021-10-01 15:44:03 -0300

    Adição do Faker Recipes no arquivo seed-recipes.

--- seed-recipes.js
+++ seed-recipes.js
@@ -64,45 +64,45 @@ async function createChefs() {
   console.log('CHEF COMPLETO: ', chefPromise);
 }
 
-// ========================================
-// async function createRecipes() {
-//   let recipes = [];
+async function createRecipes() {
+  let recipes = [];
 
-//   while (products.length < totalProducts) {
-//     products.push({
-//       category_id: Math.ceil(Math.random() * 3),
-//       user_id: usersIds[Math.floor(Math.random() * totalUsers)],
-//       name: faker.name.title(),
-//       description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
-//       old_price: faker.random.number(9999),
-//       price: faker.random.number(9999),
-//       quantity: faker.random.number(99),
-//       status: Math.round(Math.random())
-//     });
-//   }
+  while (products.length < totalProducts) {
+    products.push({
+      category_id: Math.ceil(Math.random() * 3),
+      user_id: usersIds[Math.floor(Math.random() * totalUsers)],
+      name: faker.name.title(),
+      description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
+      old_price: faker.random.number(9999),
+      price: faker.random.number(9999),
+      quantity: faker.random.number(99),
+      status: Math.round(Math.random())
+    });
+  }
 
-//   const productsPromise = products.map(product => Product.create(product));
+  const productsPromise = products.map(product => Product.create(product));
 
-//   productsIds = await Promise.all(productsPromise);
+  productsIds = await Promise.all(productsPromise);
 
-//   let files = [];
+  let files = [];
 
-//   while (files.length < 50) {
-//     files.push({
-//       name: faker.image.image(),
-//       path: `public/images/placeholder.png`,
-//       product_id: productsIds[Math.floor(Math.random() * totalProducts)]
-//     });
-//   }
+  while (files.length < 50) {
+    files.push({
+      name: faker.image.image(),
+      path: `public/images/placeholder.png`,
+      product_id: productsIds[Math.floor(Math.random() * totalProducts)]
+    });
+  }
 
-//   const filesPromise = files.map(file => File.create(file));
+  const filesPromise = files.map(file => File.create(file));
 
-//   await Promise.all(filesPromise);
-// }
+  await Promise.all(filesPromise);
+}
 
 async function init() {
   await createUsers();
   await createChefs();
+  await createRecipes();
 }
 
 init();
